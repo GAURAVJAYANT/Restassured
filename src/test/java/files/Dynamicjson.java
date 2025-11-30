26cf8dc917eeb99e22e311f8e4206fa22a158b13
GAURAVJAYANT <gaurav.jayant@groupbayport.com>
2025-11-30 14:31:36 +0530

    test a

--- src/test/java/files/Dynamicjson.java
+++ src/test/java/files/Dynamicjson.java
@@ -1,25 +1,29 @@
 package files;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
+import io.restassured.response.Response;
 import files.ReUsableMethods;
 import files.payload;
 
 public class Dynamicjson {
-    @Test
-    public void addbook() {
+
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle) {
         RestAssured.baseURI = "http://216.10.245.166";
-        String response = given().header("Content-Type", "application/json").body(payload.Addbook()).when()
-                .post("/Library/Addbook.php").then().assertThat().statusCode(200).extract().response().asString();
-        JsonPath js = ReUsableMethods.rawToJson(response);
-        String id = js.getString("ID");
+        Response resp = given().header("Content-Type", "application/json").body(payload.Addbook(isbn, aisle)).when()
+                .post("/Library/Addbook.php").then().assertThat().statusCode(200).extract().response();
+
+        JsonPath js = ReUsableMethods.rawToJson(resp.asString());
+        String id = js.get("ID");
         System.out.println(id);
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData() {
+        return new Object[][] { { "ojfwty", "9363" }, { "cwetee", "4253" }, { "okmfet", "533" } };
     }
 }
